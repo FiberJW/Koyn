6e63da5b72b35ceffa9f6d22f6b80185a688cd42
Juwan Wheatley <datwheat@gmail.com>
2017-07-03 13:49:08 -0400

    portfolio works

--- screens/screen-portfolio.js
+++ screens/screen-portfolio.js
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 import styled from "styled-components/native";
 import { inject, observer } from "mobx-react/native";
 import colors from "../config/colors";
-import PieChart from "react-native-pie-chart";
+import _ from "lodash";
 import { formatPrice } from "../components/styled/text-token-stat";
-import { Dimensions } from "react-native";
+import { Dimensions, findNodeHandle } from "react-native";
 import Ticker from "../components/ticker-prices";
-
+import { VictoryPie } from "victory-native";
 const Row = styled.View`
   margin: 16px;
   flex-direction: row;
@@ -23,6 +23,11 @@ const Input = styled.TextInput`
   text-align: right;
   border-bottom-width: 0.5px;
   border-bottom-color: ${colors.halfWhite};
+`;
+
+const Emoji = styled.Text`
+  font-size: 64px;
+  padding-vertical: 100px;
 `;
 
 const Container = styled.View`
@@ -87,40 +92,61 @@ export default class PortfolioScreen extends Component {
   render() {
     const sliceColor = [];
     const changes = [];
-    const series = this.props.prices.priceListData
+    const series = (this.props.prices.priceListData || [])
       .filter(y => {
-        return this.props.prices.portfolio[y.symbol] > 0;
+        return parseFloat(this.props.prices.portfolio[y.symbol]) > 0;
       })
       .map(k => {
         const name = k.symbol;
-        const amount = this.props.prices.portfolio[name];
+        const amount = parseFloat(this.props.prices.portfolio[name]);
         const priceAmount = k.price * amount;
+
+        console.log(priceAmount);
 
         sliceColor.push(k.color);
         changes.push(k.change);
-        return priceAmount;
+        return { name, priceAmount };
       });
 
-    const totalAmount = series.length ? series.reduce((s, v) => s + v) : 0;
+    const totalAmount = series.length
+      ? series.map(x => x.priceAmount).reduce((s, v) => s + v)
+      : 0;
 
-    return __DEV__ && this.props.prices.isLoaded // eslint-disable-line no-undef
-      ? <Kon contentContainerStyle={{ alignItems: "center" }}>
+    console.log(totalAmount);
+    console.log(this.props.prices.portfolio);
+
+    return this.props.prices.isLoaded
+      ? <Kon
+          contentContainerStyle={{
+            alignItems: "center"
+          }}
+          ref={c => (this.scrollView = c)}
+        >
           <Yuren>
             <TotalPrice>
               {formatPrice(totalAmount)}
             </TotalPrice>
-            <PieChart
-              chart_wh={Dimensions.get("window").width * 0.75}
-              series={series}
-              sliceColor={sliceColor}
-              doughnut
-              coverRadius={0.95}
-              coverFill={colors.comet}
-            />
+            {totalAmount
+              ? <VictoryPie
+                  padding={0}
+                  width={Dimensions.get("window").width * 0.7}
+                  height={Dimensions.get("window").width * 0.7}
+                  data={series}
+                  x="name"
+                  y="priceAmount"
+                  style={{
+                    data: {
+                      fill: d =>
+                        sliceColor[_.findIndex(series, o => o.name === d.name)]
+                    },
+                    labels: { fill: "transparent" }
+                  }}
+                />
+              : null}
           </Yuren>
           {this.props.prices.priceListData.map(s => {
             if (!(s.symbol in this.props.prices.portfolio)) {
-              this.props.prices.portfolio[s.symbol] = 0.0;
+              this.props.prices.portfolio[s.symbol] = "0.00";
             }
             return (
               <Row key={s.symbol}>
@@ -130,20 +156,34 @@ export default class PortfolioScreen extends Component {
                 </PriceAmount>
                 <Kol>
                   <Input
+                    ref={c => (this[`input-${s.symbol}`] = c)}
                     keyboardType="numeric"
-                    value={this.props.prices.portfolio[s.symbol].toString()}
-                    onChangeText={text => {
-                      const value = parseFloat(text).toFixed(
-                        Math.max(
-                          2,
-                          (text.toString().split(".")[1] || []).length
-                        )
-                      );
-                      if (!isNaN(parseFloat(value)) && isFinite(value)) {
-                        this.props.prices.portfolio[s.symbol] = value;
-                      } else if (text.length === 0) {
-                        this.props.prices.portfolio[s.symbol] = 0.0;
+                    onFocus={() => {
+                      if (this.scrollView) {
+                        const scrollResponder = this.scrollView.root.getScrollResponder();
+                        const inputHandle = findNodeHandle(
+                          this[`input-${s.symbol}`]
+                        );
+
+                        setTimeout(() => {
+                          scrollResponder.scrollResponderScrollNativeHandleToKeyboard(
+                            inputHandle, // The TextInput node handle
+                            100, // The scroll view's bottom "contentInset" (default 0)
+                            true // Prevent negative scrolling
+                          );
+                        }, 300);
                       }
+                    }}
+                    onLayout={({
+                      nativeEvent: { layout: { width, height, x, y } }
+                    }) => {
+                      this.setState({
+                        [`input-layout-${s.symbol}`]: { width, height, x, y }
+                      });
+                    }}
+                    value={this.props.prices.portfolio[s.symbol]}
+                    onChangeText={text => {
+                      this.props.prices.portfolio[s.symbol] = text;
                     }}
                     underlineColorAndroid="transparent"
                   />
@@ -152,6 +192,7 @@ export default class PortfolioScreen extends Component {
               </Row>
             );
           })}
+          <Emoji>💰</Emoji>
         </Kon>
       : <Container>
           <Header>oops..</Header>
